51184954eb4d4ad257a1c55cf28ebaa0bc89e40a
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-04-22 11:02:21 +0200

    add project structure

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# import sys
+
+def init_all():
+    # tutaj bedzie otwarcie pliku,
+    # stworzenie code providera i odpalenie lexera
+    # na razie zbudowane tokeny bede po prostu printowal
+    print("init_all here!")
+
+
+def main():
+    # if len(sys.argv) < 2:
+    #     print("You have to specify input file name")
+    #     return
+
+    init_all()
+
+
+if __name__ == "__main__":
+    main()
