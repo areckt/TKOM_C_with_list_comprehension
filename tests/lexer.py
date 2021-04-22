9c8459dac9fd076401736bb6dbb6818c6d316662
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-04-22 20:57:00 +0200

    add tests for keywords

--- tests/lexer.py
+++ tests/lexer.py
@@ -13,14 +13,9 @@ class LexerTest(unittest.TestCase):
         self.assertEqual(token.get_type(), TokenType.EOF_SYMBOL)
 
     def assert_expected_tokens(self, string_input, expected_sequence_of_tokens):
-        """
-        Generic method for lexer testing. Takes input and expected result as arguments.
-        Produces every possible token from given input and compares collection's items one by one.
+        # Method for lexer testing. Takes input and expected result as arguments.
+        # Produces every possible token from given input and compares collection's items one by one.
 
-        :param string_input:
-        :param expected_sequence_of_tokens:
-        :return:
-        """
         lexer = init_lexer_for_test(string_input)
         tokens = []
         token = lexer.build_and_get_token()
@@ -37,8 +32,77 @@ class LexerTest(unittest.TestCase):
     # #################
 
     def test_one_identifier(self):
-        input_str = """foo"""
+        input_str = "foo"
         expected_tokens = [Token(TokenType.IDENTIFIER, "foo")]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_many_identifiers(self):
+        input_str = "foo bar baz"
+        expected_tokens = [
+            Token(TokenType.IDENTIFIER, "foo"),
+            Token(TokenType.IDENTIFIER, "bar"),
+            Token(TokenType.IDENTIFIER, "baz")
+        ]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_return_keyword(self):
+        input_str = "return"
+        expected_tokens = [Token(TokenType.RETURN_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_while_keyword(self):
+        input_str = "while"
+        expected_tokens = [Token(TokenType.WHILE_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_if_keyword(self):
+        input_str = "if"
+        expected_tokens = [Token(TokenType.IF_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_else_keyword(self):
+        input_str = "else"
+        expected_tokens = [Token(TokenType.ELSE_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_for_keyword(self):
+        input_str = "for"
+        expected_tokens = [Token(TokenType.FOR_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_in_keyword(self):
+        input_str = "in"
+        expected_tokens = [Token(TokenType.IN_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_int_keyword(self):
+        input_str = "int"
+        expected_tokens = [Token(TokenType.INT_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_int_list_keyword(self):
+        input_str = "int[]"
+        expected_tokens = [Token(TokenType.INT_LIST_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_float_keyword(self):
+        input_str = "float"
+        expected_tokens = [Token(TokenType.FLOAT_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_float_list_keyword(self):
+        input_str = "float[]"
+        expected_tokens = [Token(TokenType.FLOAT_LIST_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_string_keyword(self):
+        input_str = "string"
+        expected_tokens = [Token(TokenType.STRING_KEYWORD)]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_string_list_keyword(self):
+        input_str = "string[]"
+        expected_tokens = [Token(TokenType.STRING_LIST_KEYWORD)]
         self.assert_expected_tokens(input_str, expected_tokens)
 
 
