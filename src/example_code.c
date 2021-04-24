d2ce1f1698b2e9e82fd4141610502d6de8ec88d3
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-04-24 13:59:44 +0200

    change example_code.c

--- src/example_code.c
+++ src/example_code.c
@@ -1,14 +1,5 @@
-int fibonacci(int n) {
-    if (n <= 2) {
-        return 1;
-    }
-    int prev = 1;
-    int result = 1;
-    int i = 0;
-    while (i < n-2) {
-        result = result + prev;
-        prev = result - prev;
-        i = i + 1;
-    }
-    return result;
+int foo(bar, baz){
+    string s = "Hello \n \t\"World\"";
+    int len = _s;
+    return len;
 }
