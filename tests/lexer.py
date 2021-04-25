5fbfecf50d7ea1514684e5530988dd9b121c0ec1
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-04-25 14:21:52 +0200

    add tests of longer code

--- tests/lexer.py
+++ tests/lexer.py
@@ -293,6 +293,121 @@ class LexerTest(unittest.TestCase):
         expected_tokens = [Token(TokenType.UNKNOWN, "list[")]
         self.assert_expected_tokens(input_str, expected_tokens)
 
+    def test_longer_code_1(self):
+        input_str = """ string foo(string name){
+                            string greeting = "Hello, " + name;
+                            return greeting;
+                        }"""
+        expected_tokens = [
+            Token(TokenType.STRING_KEYWORD),
+            Token(TokenType.IDENTIFIER, "foo"),
+            Token(TokenType.OPEN_BRACKET),
+            Token(TokenType.STRING_KEYWORD),
+            Token(TokenType.IDENTIFIER, "name"),
+            Token(TokenType.CLOSE_BRACKET),
+            Token(TokenType.OPEN_BLOCK),
+            Token(TokenType.STRING_KEYWORD),
+            Token(TokenType.IDENTIFIER, "greeting"),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.STRING_LITERAL, "Hello, "),
+            Token(TokenType.PLUS),
+            Token(TokenType.IDENTIFIER, "name"),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.RETURN_KEYWORD),
+            Token(TokenType.IDENTIFIER, "greeting"),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.CLOSE_BLOCK)
+        ]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_longer_code_2(self):
+        input_str = """ int foo(){
+                            int num = 5;
+                            string s = "";
+                            while (num > 0){
+                                s = s + "a";
+                                num = num - 1;
+                            }
+                            return _s;
+                        }"""
+        expected_tokens = [
+            Token(TokenType.INT_KEYWORD),
+            Token(TokenType.IDENTIFIER, "foo"),
+            Token(TokenType.OPEN_BRACKET),
+            Token(TokenType.CLOSE_BRACKET),
+            Token(TokenType.OPEN_BLOCK),
+            Token(TokenType.INT_KEYWORD),
+            Token(TokenType.IDENTIFIER, "num"),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.INT_LITERAL, 5),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.STRING_KEYWORD),
+            Token(TokenType.IDENTIFIER, "s"),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.STRING_LITERAL, ""),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.WHILE_KEYWORD),
+            Token(TokenType.OPEN_BRACKET),
+            Token(TokenType.IDENTIFIER, "num"),
+            Token(TokenType.GREATER),
+            Token(TokenType.INT_LITERAL, 0),
+            Token(TokenType.CLOSE_BRACKET),
+            Token(TokenType.OPEN_BLOCK),
+            Token(TokenType.IDENTIFIER, "s"),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.IDENTIFIER, "s"),
+            Token(TokenType.PLUS),
+            Token(TokenType.STRING_LITERAL, "a"),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.IDENTIFIER, "num"),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.IDENTIFIER, "num"),
+            Token(TokenType.MINUS),
+            Token(TokenType.INT_LITERAL, 1),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.CLOSE_BLOCK),
+            Token(TokenType.RETURN_KEYWORD),
+            Token(TokenType.LENGTH_OP),
+            Token(TokenType.IDENTIFIER, "s"),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.CLOSE_BLOCK),
+        ]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
+    def test_longer_code_3(self):
+        input_str = """ int[] squareList(int[] nums) {
+                            int[] result = [n*n for n in nums];
+                            return !result;
+                        }"""
+        expected_tokens = [
+            Token(TokenType.INT_LIST_KEYWORD),
+            Token(TokenType.IDENTIFIER, "squareList"),
+            Token(TokenType.OPEN_BRACKET),
+            Token(TokenType.INT_LIST_KEYWORD),
+            Token(TokenType.IDENTIFIER, "nums"),
+            Token(TokenType.CLOSE_BRACKET),
+            Token(TokenType.OPEN_BLOCK),
+            Token(TokenType.INT_LIST_KEYWORD),
+            Token(TokenType.IDENTIFIER, "result"),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.OPEN_LIST),
+            Token(TokenType.IDENTIFIER, "n"),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.IDENTIFIER, "n"),
+            Token(TokenType.FOR_KEYWORD),
+            Token(TokenType.IDENTIFIER, "n"),
+            Token(TokenType.IN_KEYWORD),
+            Token(TokenType.IDENTIFIER, "nums"),
+            Token(TokenType.CLOSE_LIST),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.RETURN_KEYWORD),
+            Token(TokenType.NOT),
+            Token(TokenType.IDENTIFIER, "result"),
+            Token(TokenType.SEMICOLON),
+            Token(TokenType.CLOSE_BLOCK),
+        ]
+        self.assert_expected_tokens(input_str, expected_tokens)
+
 
 if __name__ == '__main__':
     unittest.main()
