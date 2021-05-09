13f57906d72d37499062d26a5b5dce0db80b4e55
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-05-09 17:02:41 +0200

    add more parse methods - declarations and assignments

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,5 +1,8 @@
 from src.error_handling import ParserError, ParserSyntaxError
 from src.lexer.token import TokenType
+from src.parser.ast.complex import *
+from src.parser.ast.semi_complex import *
+from src.parser.ast.primitives import *
 from src.parser.parser_utils import ParserUtils
 
 
@@ -28,11 +31,88 @@ class Parser:
             ParserError(self.__get_position(), "UNKNOWN INSTRUCTION").warning()
         return possible_instruction
 
-    # TODO all these parse methods...
     def __parse_declaration(self):
-        pass
+        possible_type_token = self.__check_if_one_of_tokens(ParserUtils.type_tokens)
+        if not possible_type_token:
+            return None
+
+        id_token = self.__consume_token(TokenType.IDENTIFIER)
+
+        possible_function_declaration = self.__parse_function_declaration(possible_type_token, id_token)
+        if possible_function_declaration:
+            return possible_function_declaration
+
+        possible_variable_declaration = self.__parse_variable_declaration(possible_type_token, id_token)
+        if possible_variable_declaration:
+            return possible_variable_declaration
+
+        self.__consume_token(TokenType.SEMICOLON)
+        return VariableDeclaration(possible_variable_declaration, id_token)
+
+    def __parse_function_declaration(self, type_token, id_token):
+        if not self.__check_token(TokenType.OPEN_BRACKET):
+            return None
+
+        arguments = self.__parse_function_declaration_arguments()
+        self.__consume_token(TokenType.CLOSE_BRACKET)
+        self.__consume_token(TokenType.OPEN_BLOCK)
+        instructions = []
+        while not self.__check_token(TokenType.CLOSE_BLOCK):
+            instructions.append(self.__parse_instruction())
+
+        return FunctionDeclaration(type_token, id_token, arguments, instructions)
+
+    def __parse_function_declaration_arguments(self):
+        possible_type_token = self.__check_if_one_of_tokens(ParserUtils.type_tokens)
+        if not possible_type_token:
+            return []
+
+        id_token = self.__consume_token(TokenType.IDENTIFIER)
+        arguments_so_far = [FunctionArgument(possible_type_token, id_token)]
+        while self.__check_token(TokenType.COMMA):
+            type_token = self.__consume_one_of_tokens(ParserUtils.type_tokens, "type token")
+            id_token = self.__consume_token(TokenType.IDENTIFIER)
+            arguments_so_far.append(FunctionArgument(type_token, id_token))
+
+        return arguments_so_far
+
+    def __parse_variable_declaration(self, possible_type_token, id_token):
+        if not self.__check_token(TokenType.ASSIGN):
+            return None
+
+        value = self.__parse_r_value()
+        self.__consume_token(TokenType.SEMICOLON)
+        return VariableDeclaration(possible_type_token, id_token, value)
 
     def __parse_id_starting(self):
+        possible_id_token = self.__check_token(TokenType.IDENTIFIER)
+        if not possible_id_token:
+            return None
+
+        possible_assignment = self.__parse_assignment(possible_id_token)
+        if possible_assignment:
+            return possible_assignment
+
+        possible_function_invocation = self.__parse_function_invocation(possible_id_token)
+        if possible_function_invocation:
+            return possible_function_invocation
+
+        ParserError(self.__get_position(), f'INVALID TOKEN AFTER ID: {self.__get_current_token()}!').fatal()
+
+    def __parse_assignment(self, id_token):
+        if not self.__check_token(TokenType.ASSIGN):
+            return None
+
+        value = self.__parse_r_value()
+        self.__consume_token(TokenType.SEMICOLON)
+
+        return VariableAssignment(id_token, value)
+
+    def __parse_r_value(self):
+        return self.__parse_arithmetic_expression()  # TODO condition
+
+    # TODO all these parse methods...
+    def __parse_arithmetic_expression(self):
         pass
 
     def __parse_return(self):
