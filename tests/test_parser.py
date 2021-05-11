40328602569bf902b1934ce97f311b619a5abc2b
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-05-11 14:38:24 +0200

    add more tests - declaring lint, lfloat, lstring

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,6 +15,7 @@ class ParserTest(unittest.TestCase):
             current_expected = list_of_expected[i]
             self.assertTrue(isinstance(current_instruction, current_expected))
 
+    # INT DECLARATION
     def test_int_variable_declaration(self):
         input_str = """int i = 5;"""
         list_of_expected = [VariableDeclaration]
@@ -26,9 +27,17 @@ class ParserTest(unittest.TestCase):
         input_str = """int i = 5;"""
         parser = init_parser_for_test(input_str)
         program = parser.parse_program()
-        var_type = program[0].type
-        self.assertTrue(isinstance(var_type, Type))
+        var_type = program[0].type.type
+        self.assertTrue(var_type == Types.INT)
 
+    def test_value_in_int_variable_declaration(self):
+        input_str = """int i = 5;"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        value = program[0].value.get_value()
+        self.assertEqual(value, 5)
+
+    # FLOAT DECLARATION
     def test_float_variable_declaration(self):
         input_str = """float temp = 36.5;"""
         list_of_expected = [VariableDeclaration]
@@ -40,9 +49,17 @@ class ParserTest(unittest.TestCase):
         input_str = """float temp = 36.5;"""
         parser = init_parser_for_test(input_str)
         program = parser.parse_program()
-        var_type = program[0].type
-        self.assertTrue(isinstance(var_type, Type))
+        var_type = program[0].type.type
+        self.assertTrue(var_type == Types.FLOAT)
 
+    def test_value_in_float_variable_declaration(self):
+        input_str = """float temp = 36.5;"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        value = program[0].value.get_value()
+        self.assertEqual(value, 36.5)
+
+    # STRING DECLARATION
     def test_string_variable_declaration(self):
         input_str = """string word = "great!";"""
         list_of_expected = [VariableDeclaration]
@@ -54,9 +71,17 @@ class ParserTest(unittest.TestCase):
         input_str = """string word = "great!";"""
         parser = init_parser_for_test(input_str)
         program = parser.parse_program()
-        var_type = program[0].type
-        self.assertTrue(isinstance(var_type, Type))
+        var_type = program[0].type.type
+        self.assertTrue(var_type == Types.STRING)
 
+    def test_value_in_string_variable_declaration(self):
+        input_str = """string word = "great!";"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        value = program[0].value.get_value()
+        self.assertEqual(value, "great!")
+
+    # DECLARATION OF VARIABLE WITH FORBIDDEN ID
     def test_variable_declaration_with_forbidden_id_1(self):
         input_str = """int for = 5;"""
         parser = init_parser_for_test(input_str)
@@ -92,6 +117,72 @@ class ParserTest(unittest.TestCase):
         parser = init_parser_for_test(input_str)
         with self.assertRaises(Exception):
             parser.parse_program()
+
+    # LINT DECLARATION
+    def test_lint_list_declaration(self):
+        input_str = """lint i = [1, 2, 3];"""
+        list_of_expected = [ListVariableDeclaration]
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        self.assert_instructions(program, list_of_expected)
+
+    def test_type_in_lint_list_declaration(self):
+        input_str = """lint i = [1, 2, 3];"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        var_type = program[0].type.type
+        self.assertEqual(var_type, ListTypes.LINT)
+
+    def test_values_in_lint_list_declaration(self):
+        input_str = """lint i = [1, 2, 3];"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        values = [literal.get_value() for literal in program[0].values]
+        self.assertEqual(values, [1, 2, 3])
+
+    # LFLOAT DECLARATION
+    def test_lfloat_list_declaration(self):
+        input_str = """lfloat temps = [36.7, 36.2, 37.1];"""
+        list_of_expected = [ListVariableDeclaration]
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        self.assert_instructions(program, list_of_expected)
+
+    def test_type_in_lfloat_list_declaration(self):
+        input_str = """lfloat temps = [36.7, 36.2, 37.1];"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        var_type = program[0].type.type
+        self.assertEqual(var_type, ListTypes.LFLOAT)
+
+    def test_values_in_lfloat_list_declaration(self):
+        input_str = """lfloat temps = [36.7, 36.2, 37.1];"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        values = [literal.get_value() for literal in program[0].values]
+        self.assertEqual(values, [36.7, 36.2, 37.1])
+
+    # LSTRING DECLARATION
+    def test_lstring_list_declaration(self):
+        input_str = """lstring chars = ["a", "b", "c"];"""
+        list_of_expected = [ListVariableDeclaration]
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        self.assert_instructions(program, list_of_expected)
+
+    def test_type_in_lstring_list_declaration(self):
+        input_str = """lstring chars = ["a", "b", "c"];"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        var_type = program[0].type.type
+        self.assertEqual(var_type, ListTypes.LSTRING)
+
+    def test_values_in_lstring_list_declaration(self):
+        input_str = """lstring chars = ["a", "b", "c"];"""
+        parser = init_parser_for_test(input_str)
+        program = parser.parse_program()
+        values = [literal.get_value() for literal in program[0].values]
+        self.assertEqual(values, ["a", "b", "c"])
 
 
 if __name__ == '__main__':
