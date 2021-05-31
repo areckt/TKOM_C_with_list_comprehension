458c2a56abd88663a3390e4faa969a4ade0a33ff
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-05-31 15:59:21 +0200

    add exception classes

---
+++ src/interpreter/exceptions.py
@@ -0,0 +1,36 @@
+class DivisionError(Exception):
+    def __init__(self):
+        super().__init__()
+
+
+class InvalidSyntax(Exception):
+    def __init__(self, position=(0, 0), expected_type=None, given_type=None, given_value=None):
+        self.position = position
+        self.expected_type = expected_type
+        self.given_type = given_type
+        self.given_value = given_value
+
+
+class InvalidValue(Exception):
+    def __init__(self, expected_type, given_type):
+        self.expected_type = expected_type
+        self.given_type = given_type
+
+
+class UndefinedOperation(Exception):
+    def __init__(self, left_operand, operator, right_operand):
+        self.left_operand = left_operand
+        self.operator = operator
+        self.right_operand = right_operand
+
+
+class InvalidOperation(Exception):
+    def __init__(self, left_operand, operator, right_operand):
+        self.left_operand = left_operand
+        self.operator = operator
+        self.right_operand = right_operand
+
+
+class Undeclared(Exception):
+    def __init__(self):
+        super().__init__()
