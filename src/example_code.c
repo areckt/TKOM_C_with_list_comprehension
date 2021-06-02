85f4e405d1a41d15972ff7e94cf4b26ce90024bb
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-06-02 23:51:10 +0200

    change example code

--- src/example_code.c
+++ src/example_code.c
@@ -4,13 +4,16 @@ int main(){
     }
 
     int i = 1;
-    while(i < 15){
-        if(i<10){
+    while (i < 15)
+    {
+        if (i < 10)
+        {
             i = i+10;
             string str = "example";
-            }
-        else{
-            fun(1,2,3);
+        }
+        else
+        {
+            return fun(1, 2, 3);
         }
     }
     return 0;
