f25d82d11fc05605b28e171158c62878e969166b
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-06-03 13:33:48 +0200

    change example code

--- src/example_code.c
+++ src/example_code.c
@@ -1,20 +1,48 @@
-int main(){
-    int fun(int a, int b, int c){
-        return a+b+c;
+int main() {
+    int a = 5;                              # int declaration
+    a = 3*(a + 2 * (1 - a));                # int assignment
+
+    float b = 4.5;                          # float declaration
+    b = b / (b * 0.1 + 2.4);                # float assignment
+
+    string c = "Hello";                     # string declaration
+    c = c + ", World";                      # string assignment
+
+    lint d = [1, 2, 3];                     # lint declaration
+    d = [a, d[0], 2*4];                     # lint assignment
+
+    lint e = [num * num for num in d];      # lint list comprehension declaration
+    e = [-2 * num for num in e];            # lint list comprehension assignment
+
+    lfloat f = [1.1, -4.2, 2.4];            # lfloat declaration
+    f = [f[a-4], 0.4, 5.5];                 # lfloat assignment
+
+    lfloat g = [b * fnum for fnum in f];    # lfloat list comprehension declaration
+    g = [9.9 * fnum for fnum in g];         # lfloat list comprehension assignment
+
+    lstring h = ["Hello", ",", " "];        # lstring declaration
+    h = [h[0], h[1], h[2], "World"];        # lstring assignment
+
+    lstring i = ["Hey"+s for s in h];       # lstring list comprehension declaration
+    i = [word + "!" for word in i];         # lstring list comprehension assignment
+
+    if (a < 10) {                           # if statement
+        return a;
+    }
+    else {                                  # else statement
+        a = a + a;
     }
 
-    int i = 1;
-    while (i < 15)
-    {
-        if (i < 10)
-        {
-            i = i+10;
-            string str = "example";
-        }
-        else
-        {
-            return fun(1, 2, 3);
-        }
+    while (a > 5) {                         # while statement
+        a = a - 1;
     }
-    return 0;
+
+    lfloat foo(float x, float y, float z){  # function declaration inside main with 3 arguments
+        lfloat result = [x, y, z];          # use of arguments to declare new variable
+        return result;                      # return variable with matching type (lfloat foo => return lfloat)
+    }
+
+    int len = _foo(4.4, b * 5.5, 6.6);      # length of foo() result assigned to int
+
+    return len;                             # return variable with matching type (int main => return int)
 }
