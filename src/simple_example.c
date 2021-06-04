31cb08d8c35a54e8d242be14adbcbda1c71d64d4
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-06-04 14:08:47 +0200

    calculate Fibonacci number in simple_example

--- src/simple_example.c
+++ src/simple_example.c
@@ -1,4 +1,17 @@
 int main(){
-    int a = 10;
-    return a;
+    int n = 10;
+    if (n <= 2){
+        return 1;
+    }
+
+    int prev = 1;
+    int result = 1;
+    int i = 0;
+
+    while (i < n-2) {
+        result = result + prev;
+        prev = result - prev;
+        i = i + 1;
+    }
+    return result;
 }
