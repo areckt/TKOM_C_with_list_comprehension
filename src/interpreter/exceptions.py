826a1a3a2f01daa587e902aef57123a71903f3bb
Arkadiusz Dawid <01143540@pw.edu.pl>
2021-06-04 16:35:50 +0200

    cleanup

--- src/interpreter/exceptions.py
+++ src/interpreter/exceptions.py
@@ -3,40 +3,6 @@ class DivisionError(Exception):
         super().__init__()
 
 
-# class InvalidSyntax(Exception):
-#     def __init__(self, position=(0, 0), expected_type=None, given_type=None, given_value=None):
-#         self.position = position
-#         self.expected_type = expected_type
-#         self.given_type = given_type
-#         self.given_value = given_value
-
-
-# class InvalidValue(Exception):
-#     def __init__(self, expected_type, given_type):
-#         self.expected_type = expected_type
-#         self.given_type = given_type
-
-
-class UndefinedOperation(Exception):
-    def __init__(self, left_operand, operator, right_operand):
-        self.left_operand = left_operand
-        self.operator = operator
-        self.right_operand = right_operand
-
-
-class InvalidOperation(Exception):
-    def __init__(self, left_operand, operator, right_operand):
-        self.left_operand = left_operand
-        self.operator = operator
-        self.right_operand = right_operand
-
-
-# class Undeclared(Exception):
-#     def __init__(self, subject, name):
-#         self.subject = subject
-#         self.name = name
-
-
 class IndexOutOfRange(Exception):
     def __init__(self):
         super().__init__()
